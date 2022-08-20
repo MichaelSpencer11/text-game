502a3a8ea6bc5ab6d3c0d41fbd356354712eefe6
MichaelSpencer11 <mspencer009@gmail.com>
2022-08-19 17:00:36 -0700

    wrote some battle stuff, finished player physical attack

--- Spell.java
+++ Spell.java
@@ -1,19 +1,7 @@
 package textgame;
 
-public class Spell {
+public class Spell extends Action {
 	
-	protected String type;
-	protected int mp;
-	protected int spellPower;
-	protected int hitRate;
-	protected boolean reflectable;
-	protected boolean absorbedByRunic;
-	protected String element;
-	protected boolean physicalDamage;
-	protected boolean ignoresDefense;
-	protected boolean ignoresMBlock;
-	protected String targets;
-	protected String description;
-	protected String effect;
+	public Spell
 	
 }
